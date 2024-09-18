8f967ab1d0a1acca325cf66ac434356c9c7722f7
Ahmed <ahmedalkhalaf@outlook.fr>
2024-09-18 12:29:32 +0200

    CRUD

--- backend/models/book.js
+++ backend/models/book.js
@@ -11,7 +11,7 @@ const thingSchema = mongoose.Schema({
     "userId" : { type: String, required: true },
     "grade": { type: Number, required: true }
   }],
-
+  averageRating: { type: Number }
 });
 
 module.exports = mongoose.model('Book', thingSchema);
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const auth = require('../middleware/auth');
 const router = express.Router();
+//const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 const booksCtrl = require('../controllers/books');
 
 
-router.post('/', booksCtrl.createBook);
-router.put('/:id', auth, booksCtrl.modifyBook);
-router.delete('/:id', auth, booksCtrl.deleteBook);
-router.get('/:id', auth, booksCtrl.getOneBook);
+router.post('/', multer, booksCtrl.createBook);
+router.put('/:id', multer, booksCtrl.modifyBook);
+router.delete('/:id', booksCtrl.deleteBook);
+router.get('/:id', booksCtrl.getOneBook);
 router.get('/', booksCtrl.getAllBooks)
+router.post('/:id/rating', booksCtrl.rateBook)
+router.get('/bestrating', booksCtrl.getBestRatedBooks);
 
 
 module.exports = router;
