f8733a9a11b3620e75124127f814b69f02f2a0ca
Ahmed <ahmedalkhalaf@outlook.fr>
2024-09-20 15:26:23 +0200

    Sharp

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,12 +1,23 @@
 const multer = require('multer');
 
-const MIME_TYPES = {
+  const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png'
 };
 
-const storage = multer.diskStorage({
+const storage = multer.memoryStorage();
+
+
+module.exports = multer({ storage: storage }).single('image');
+
+
+
+
+
+
+
+/*const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images');
   },
@@ -15,6 +26,4 @@ const storage = multer.diskStorage({
     const extension = MIME_TYPES[file.mimetype];
     callback(null, name + Date.now() + '.' + extension);
   }
-});
-
-module.exports = multer({storage: storage}).single('image');
+});*/
