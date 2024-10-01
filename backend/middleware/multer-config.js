b5e8640446870666b4625d5a2c727246ab3e34f1
Ahmed <ahmedalkhalaf@outlook.fr>
2024-10-01 12:45:10 +0200

    /

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -17,13 +17,3 @@ module.exports = multer({ storage: storage }).single('image');
 
 
 
-/*const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'images');
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
-  }
-});*/
